51ad773d06466088aca8af68102ae196e4878275
Def1ss <borsuk.daniil7@gmail.com>
2026-06-04 15:57:30 +0300

    static_root remake

--- IGI/IGI_LR5/forsazh_fitness/settings.py
+++ IGI/IGI_LR5/forsazh_fitness/settings.py
@@ -84,7 +84,7 @@ LANGUAGE_CODE = 'ru-ru'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_ROOT = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
